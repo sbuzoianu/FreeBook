0cfa7dfe42c7ab0d00f97cca2909b75a085f3c78
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2022-08-12 12:08:21 +0300

    Add project files.

---
+++ DatabaseHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+
+namespace FreeBook
+{
+    class DatabaseHelper
+    {
+        public static void Initialisation(string connectionString)
+        {
+            ClearDatabase(connectionString);
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string filePath = "C://student//2022-2023//Lucru_OJTI//FreeBook//Resurse//carti.txt";
+                string cmdText = "Insert into carti (titlu,autor,gen) values (@titlu,@autor,@gen);";
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    int i = 0;
+                    while (reader.Peek() >= 0)
+                    {
+                        i++;
+                        var line = reader.ReadLine().Split('*');
+                        using (SqlCommand cmd = new SqlCommand(cmdText, con))
+                        {
+                            //   cmd.Parameters.AddWithValue("id", i);
+                            cmd.Parameters.AddWithValue("titlu", line[0]);
+                            cmd.Parameters.AddWithValue("autor", line[1]);
+                            cmd.Parameters.AddWithValue("gen", line[2]);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void ClearDatabase(string connectionString)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string cmdText = "Delete from carti";
+                ExecuteSQLQuery(con, cmdText);
+                cmdText = "Delete from utilizatori";
+                ExecuteSQLQuery(con, cmdText);
+                cmdText = "Delete from imprumut";
+                ExecuteSQLQuery(con, cmdText);
+            }
+        }
+
+        private static void ExecuteSQLQuery(SqlConnection con, string cmdText)
+        {
+            using (SqlCommand cmd = new SqlCommand(cmdText, con))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ SqlDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeBook
+{
+    class SqlDataAccess
+    {
+        public static string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\student\2022-2023\Lucru_OJTI\FreeBook\FreeBookDB.mdf;Integrated Security=True";
+
+    }
+}
